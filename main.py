6f195fe5ad0b6ceafbebf1a0c78f68f4cefa6ca9
Okamiryuku <okamiryuku@gmail.com>
2023-11-01 22:35:52 -0600

    about text changes

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
-from flask_ckeditor import CKEditor
-from functools import wraps
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-ckeditor = CKEditor(app)
 Bootstrap5(app)
 
 
@@ -17,4 +13,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
