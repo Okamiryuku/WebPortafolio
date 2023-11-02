3403c6fa90d71ae1684b8ccf331c658ef4b94478
Okamiryuku <okamiryuku@gmail.com>
2023-11-02 09:08:16 -0600

    Restructure of the web page, changing some sections

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 
 
@@ -7,10 +7,16 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        message = request.form.get('message')
+        print(name, email, subject, message)
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
