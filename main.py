02449dd44247782f3dbd8a7c0f4b3307bcd990e8
Okamiryuku <okamiryuku@gmail.com>
2023-11-02 20:36:44 -0600

    creating a db

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contact_form.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    subject = db.Column(db.String(100))
+    message = db.Column(db.Text)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -14,6 +32,12 @@ def home():
         email = request.form.get('email')
         subject = request.form.get('subject')
         message = request.form.get('message')
+
+        contact_entry = Contact(name=name, email=email, subject=subject, message=message)
+
+        db.session.add(contact_entry)
+        db.session.commit()
+
     return render_template("index.html")
 
 
